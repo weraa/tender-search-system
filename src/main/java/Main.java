fe44454c47053eeb7739475ab1a9a7ceb625e909
korneliarohullko <korneliarohulko@gmail.com>
2018-06-08 02:38:44 +0200

    Maven build setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import com.generated.BZPPublicWebService;
+import com.generated.BZPPublicWebServiceHttpPost;
+import com.generated.BZPPublicWebServiceHttpPostImpl;
+
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        BZPPublicWebServiceHttpPost bzpPublicWebServiceHttpPost = new BZPPublicWebServiceHttpPostImpl();
+        int res = bzpPublicWebServiceHttpPost.ileOgloszenK4();
+        System.out.println(res);
+    }
 }
